4de49647aa345176bca5e7eee33bfc14a90d7af2
Erez Karpas <karpase@gmail.com>
2023-06-19 12:56:50 +0300

    exposed as ROS service

--- map_cover/predict_node.py
+++ map_cover/predict_node.py
@@ -1,20 +1,20 @@
 import rclpy
 from rclpy.node import Node
-
-from sensor_msgs.msg import Image
-import map_cover.predict
+import map_cover.predict, map_cover.map, map_cover.grid
 import cv2
-import cv_bridge
-
 from ament_index_python.packages import get_package_share_directory
 import os
+from swarm_interfaces.srv import MapToGrid
+from swarm_interfaces.msg import MapWithCoords, Zone, GeoPoint, ZoneShapeEnum, ZoneTypeEnum
+
+
 # may raise PackageNotFoundError
 package_share_directory = get_package_share_directory('map_cover')
 
-class MapCoverNode(Node):
+class MapToGridServiceNode(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('MapToGrid')
         
         self.declare_parameter('model_name', os.path.join(get_package_share_directory('map_cover'), 'resource', 'v7-LandCover-retrained-twice'))
         model_name = self.get_parameter('model_name').value
@@ -23,36 +23,57 @@ class MapCoverNode(Node):
         self.declare_parameter('cover_distributions_filename', os.path.join(get_package_share_directory('map_cover'), 'resource', 'cover_distribution.yaml'))
         cover_distributions_filename = self.get_parameter('cover_distributions_filename').value
         self.cover_distributions = map_cover.predict.load_cover_distributions(cover_distributions_filename)
+        
+        self.srv = self.create_service(MapToGrid, 'map_to_grid', self.map_to_grid_callback)        
 
-        self.bridge = cv_bridge.CvBridge()
-        self.subscription = self.create_subscription(
-            Image,
-            'map_cover',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
-        self.map_cover_publisher = self.create_publisher(Image, "map_cover_labels", 10)  
+    def map_to_grid_callback(self, request, response):
+        self.get_logger().info('got request: ' +  request.input_map.map_filename)
 
-    def listener_callback(self, msg):
-        self.get_logger().info('got message')
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        output = map_cover.predict.image_to_pixel_cover(self.model, cv_image)   
-        cv2.imwrite("kaka.jpeg", output)
-        image2 = cv2.imread("kaka.jpeg")     
-        self.map_cover_publisher.publish(self.bridge.cv2_to_imgmsg(image2, "bgr8"))
+        # Load Map from request        
+        map = map_cover.map.Map(request.input_map.bottom_left.longitude, request.input_map.top_right.longitude,
+                                request.input_map.bottom_left.latitude, request.input_map.top_right.latitude,
+                                request.input_map.map_filename)            
+        self.get_logger().info('map loaded, predicting cover labels')
+
+        pred = map_cover.predict.image_to_pixel_cover(self.model, map.image)
+        map_pred = map_cover.map.Map(0, 100, 0, 100, pred)
+
+        self.get_logger().info('cover labels predicted, adding polygons')
+        cover_distributions = self.cover_distributions.copy()
+        polygon_id = map_cover.predict.NUM_CLASSES
+        for zone in request.additional_polygons:
+            polygon = [(point.longitude, point.latitude) for point in zone.geo_points]
+            dist = map_cover.grid.DiscreteProbabilityDistribution(
+                {zone.target_data.target_priority[i] : zone.target_data.target_detection_probability[i] for i in range(len(zone.target_data.target_priority))})
+            map_pred.color_polygon(polygon, polygon_id)   
+            cover_distributions[polygon_id] = dist
+            polygon_id = polygon_id + 1
+
+        self.get_logger().info('added polygons, overlaying grid')
+        grid = map_pred.to_grid(request.grid_cell_size, request.grid_cell_size, cover_distributions)
+
+        for i,j in grid:            
+            z = Zone()
+            z.geo_points = [GeoPoint(longitude=x, latitude=y, altitude=0.0) for x,y in grid[i,j].area.exterior.coords]
+            z.target_data.target_priority = grid[i,j].target_data.values
+            z.target_data.target_detection_probability = [grid[i,j].target_data[val] for val in grid[i,j].target_data.values]
+            z.zone_type.value = ZoneTypeEnum.TARGET_DATA_ZONE
+            z.zone_shape.value = ZoneShapeEnum.POLYGON
+            response.output_grid.append(z)
+        return response
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MapCoverNode()
+    map_to_grid_node = MapToGridServiceNode()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(map_to_grid_node)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    map_to_grid_node.destroy_node()
     rclpy.shutdown()
 
 
--- map_cover/test_predict_node.py
+++ map_cover/test_predict_node.py
@@ -1,42 +1,56 @@
 import rclpy
 from rclpy.node import Node
+import sys
 
-from sensor_msgs.msg import Image
-import cv2
-import cv_bridge
+from swarm_interfaces.srv import MapToGrid
+from swarm_interfaces.msg import MapWithCoords, Zone, GeoPoint, ZoneShapeEnum, ZoneTypeEnum, MissionTargetData
 
-class MinimalPublisher(Node):
 
+def geopoint_from_xy(x, y):
+    return GeoPoint(latitude = float(x), longitude = float(y), altitude = 0.0)
+
+class MapToGridClientAsync(Node):
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.declare_parameter('filename', 'kaka_input.tif')
-        self.publisher_ = self.create_publisher(Image, 'map_cover', 10)
-        timer_period = 3  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('MapToGrid_client')
+        self.cli = self.create_client(MapToGrid, 'map_to_grid')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = MapToGrid.Request()
 
-    def timer_callback(self):
-        filename = self.get_parameter('filename').value
-        cv_image = cv2.imread(filename)
-        bridge = cv_bridge.CvBridge()
-        
-        msg = bridge.cv2_to_imgmsg(cv_image, "bgr8")        
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing')
-        self.i += 1
+    def send_request(self, filename):
+        self.req.input_map.map_filename = filename
+        self.req.input_map.bottom_left = geopoint_from_xy(0, 0)
+        self.req.input_map.top_right = geopoint_from_xy(100, 100)
+
+        polygon1 = Zone()
+        polygon1.geo_points = [geopoint_from_xy(5,5), geopoint_from_xy(15, 15), geopoint_from_xy(15, 5)]
+        polygon1.target_data.target_priority = [1,5]
+        polygon1.target_data.target_detection_probability = [0.8, 0.2]
+
+        polygon2 = Zone()
+        polygon2.geo_points = [geopoint_from_xy(60, 60), geopoint_from_xy(60, 90), geopoint_from_xy(90, 90), geopoint_from_xy(90, 60)]
+        polygon2.target_data.target_priority = [1,5, 10]
+        polygon2.target_data.target_detection_probability = [0.2, 0.3, 0.5]
+
+        self.req.additional_polygons = [polygon1, polygon2]
+
+        self.req.grid_cell_size = 10.0
+
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    minimal_client = MapToGridClientAsync()
+    response = minimal_client.send_request(sys.argv[1])
+    minimal_client.get_logger().info(
+        'Result:  ' +  str(response))        
 
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    minimal_client.destroy_node()
     rclpy.shutdown()
 
 
